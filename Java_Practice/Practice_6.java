31d93771930bd85a6dcc1e07c45832c9bce184b3
Brian Atkinson <76421912+BrianAtkinson93@users.noreply.github.com>
2021-05-17 23:30:07 -0700

    Practice_5 - Converting Strings to numbers

---
+++ Java_Practice/Practice_6.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        // Implicit Casting
+        // byte > short > int > long > float > double
+        String x = "1.1";
+        double y = Double.parseDouble(x) + 2;
+        System.out.println(y);
+
+    }
+}
